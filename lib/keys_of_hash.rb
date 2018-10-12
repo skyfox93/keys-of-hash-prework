106aba7e3ca6ea6b035f59491e0ff8b533807819
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-12 15:35:19 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    new_keys=[]
+    args.each {|arg|
+    values.each_with_index {|val,index|
+      if val==arg 
+        new_keys << keys[index]
+      end
+    }
+  }
+new_keys
   end
 end
+
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+
+puts animals.keys_of("Madagascar")
